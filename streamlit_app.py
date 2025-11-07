96f459a4b01558ecc501e850796f4d459fecad84
Capa-TY <capa20031016@gmail.com>
2025-11-07 17:01:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -175,26 +175,6 @@ with col1:
     else:
         st.warning("無可顯示的站點，請檢查過濾條件！")
 
-    
-
-    df = df.dropna(subset=['latitude', 'longitude', 'available_rent_bikes'])
-
-    # 使用 Plotly Express 來繪製熱力圖
-    fig = px.density_mapbox(df, 
-                            lat="latitude", 
-                            lon="longitude", 
-                            z="available_rent_bikes", 
-                            radius=10,  # 圓點大小
-                            center=dict(lat=df['latitude'].mean(), lon=df['longitude'].mean()),  # 地圖中心
-                            zoom=12,  # 地圖縮放
-                            color_continuous_scale="Viridis",  # 熱力圖的顏色
-                            title="YouBike 站點可借車數熱力圖")
-    
-    # 設定 Mapbox 風格
-    fig.update_layout(mapbox_style="open-street-map")
-    
-    # 顯示圖表
-    fig.show()
 
 with col2:
     pass  # 空白欄
@@ -243,7 +223,7 @@ with col3:
 
     # 顯示收藏清單
     if st.session_state.favorites:
-        st.write("你的收藏站點：")
+        st.write("你的收藏站點：(可自行複製前往站點搜尋)")
         for s in st.session_state.favorites:
             st.write("•", s)
             
